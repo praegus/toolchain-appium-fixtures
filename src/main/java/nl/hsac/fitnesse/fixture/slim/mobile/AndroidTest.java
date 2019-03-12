04bfea30701b76817eb9a79dd51975d6448cd2f6
Tom Heintzberger <tom.heintzberger@gmail.com>
2019-03-12 10:58:03 +0100

    press all the enterz

--- src/main/java/nl/hsac/fitnesse/fixture/slim/mobile/AndroidTest.java
+++ src/main/java/nl/hsac/fitnesse/fixture/slim/mobile/AndroidTest.java
@@ -2,6 +2,8 @@ package nl.hsac.fitnesse.fixture.slim.mobile;
 
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.AndroidElement;
+import io.appium.java_client.android.nativekey.AndroidKey;
+import io.appium.java_client.android.nativekey.KeyEvent;
 import nl.hsac.fitnesse.fixture.util.mobile.AndroidHelper;
 
 /**
@@ -14,6 +16,12 @@ public class AndroidTest extends MobileTest<AndroidElement, AndroidDriver<Androi
 
 	public AndroidTest(int secondsBeforeTimeout) {
 		super(secondsBeforeTimeout);
+	}
+
+	@Override
+	public boolean pressEnter() {
+		getMobileHelper().driver().pressKey(new KeyEvent(AndroidKey.NUMPAD_ENTER));
+		return true;
 	}
 
 	@Override
