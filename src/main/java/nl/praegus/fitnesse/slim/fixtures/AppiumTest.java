64963bbdaf857a3598725ec7d7a6525391b99c3f
Tom Heintzberger <tom.heintzberger@gmail.com>
2019-12-23 16:54:20 +0100

    Catch unsupportedcommandexception

--- src/main/java/nl/praegus/fitnesse/slim/fixtures/AppiumTest.java
+++ src/main/java/nl/praegus/fitnesse/slim/fixtures/AppiumTest.java
@@ -25,12 +25,7 @@ import nl.praegus.fitnesse.slim.util.AppiumHelper;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.lang3.exception.ExceptionUtils;
 import org.apache.commons.text.StringEscapeUtils;
-import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
-import org.openqa.selenium.TimeoutException;
-import org.openqa.selenium.UnhandledAlertException;
-import org.openqa.selenium.WebDriverException;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.*;
 import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.Select;
 
